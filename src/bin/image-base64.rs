67f3632f6c39f851f5a5cbe16c2f7187703a71db
Andhika M. Wijaya <okumaru07@gmail.com>
2024-07-12 23:30:24 +0700

    fix gen image to base64

--- src/bin/image-base64.rs
+++ src/bin/image-base64.rs
@@ -1,13 +1,19 @@
+use std::io::Cursor;
+
 extern crate image;
 
+use base64::{engine::general_purpose, Engine};
+use image::{ImageBuffer, Rgba};
+
 fn main() {
-    let image: ImageBuffer<Rgba<u8>, Vec<u8>> = image::open("/home/dika/Pictures/Screenshots/1.png").unwrap().into();
+    let image: ImageBuffer<Rgba<u8>, Vec<u8>> = image::open("C:/Users/Dika/Documents/github/warehouse/assets/soh.png").unwrap().into();
 
     let mut buf: Vec<u8> = Vec::new();
     image
         .write_to(&mut Cursor::new(&mut buf), image::ImageFormat::Png)
         .expect("Couldn't write image to bytes.");
-        
+
     let b64 = general_purpose::STANDARD.encode(buf);
-    format!("{}", b64);
+
+    dbg!("{}", b64);
 }
